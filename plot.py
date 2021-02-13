c861ae3aa2c8fe8268a8aa6e86758284950ca297
Tiago Teles <74304258+TiagoTeles@users.noreply.github.com>
2021-02-13 02:11:33 +0100

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Script used to plot the orbits of space debris.
+
+This script takes in a .csv file and plots the
+orbital properties of space debris as a function
+of time. This scripts is used in conjuction with
+__.py, which generates the required .csv files.
+"""
+
+# Auhorship
+__author__ = "Tiago Teles"
+__credits__ = ["Tiago Teles", "Ron Noomen"]
+__license__ = "GPL"
+__email__ = "tmoreiradafont@tudelft.net"
+
+# Imports
+
+# Constants
+
+# Variables
+
+# Function Definitions
+
+# Main Program
