5e2999245b0c20bd01f9d6070b7d2e8f9cace994
Tiago Teles <teletiago1@hotmail.com>
2021-03-12 01:46:40 +0100

    Update file structure

---
+++ src/__about__.py
@@ -0,0 +1,9 @@
+__author__ = "Tiago Teles"
+__credits__ = ["Tiago Teles", "Ron Noomen"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Tiago Teles"
+__email__ = "tmoreiradafont@tudelft.net"
+__status__ = "Prototype"
+
+__all__ = [__author__, __credits__, __license__, __version__, __maintainer__, __email_, __status__]
