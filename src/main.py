12a58984a52f2027fef47b5e8b7632cd7e634ffa
Tiago Teles <teletiago1@hotmail.com>
2021-06-03 23:17:38 +0200

    Restructured file structure

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# ============================================================
+# Name: Tiago Teles
+# License: GNU GPL 3.0
+# Version: 0.0.1
+# Email: T.MoreiraDaFonteFonsecaTeles@student.tudelft.nl
+# ============================================================
+
+# Function Definitions
+def hello_world():
+    print("Hello World!")
+
+# Main Program
+if __name__ == "__main__":
+    hello_world()
---
+++ tests/main.py
@@ -0,0 +1,14 @@
+# ============================================================
+# Name: Tiago Teles
+# License: GNU GPL 3.0
+# Version: 0.0.1
+# Email: T.MoreiraDaFonteFonsecaTeles@student.tudelft.nl
+# ============================================================
+
+# python -m unittest tests.main
+
+# Imports
+from src import main
+
+# Main Program
+main.hello_world()
